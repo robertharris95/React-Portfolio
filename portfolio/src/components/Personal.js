04a08c4d04b40f522448bf40ed4f2686c884ce38
Robert Harris <rlh9@rice.edu>
2020-04-07 00:30:22 -0500

    All pages completed

--- portfolio/src/components/Personal.js
+++ portfolio/src/components/Personal.js
@@ -2,6 +2,136 @@ import React from "react";
 function Personal() {
     return (
         <>
+ <>
+    <main className="container">
+
+    <div className="row" >
+        <p className="Project-headings">Small Projects</p>
+    </div>
+
+
+    <div className="row">
+    <div className="card" style={{width: "18rem"}}>
+    <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+        <h5 className="card-title">Twitter Bot</h5>
+        <p className="card-text">This content is coming soon!</p>
+        <a href="#" className="btn btn-primary">View Here</a>
+    </div>
+    </div>
+
+    <div className="card" style={{width: "18rem"}}>
+        <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+        <h5 className="card-title">Weather Bot</h5>
+        <p className="card-text">This content is coming soon!</p>
+        <a href="#" className="btn btn-primary">View Here</a>
+        </div>
+    </div>
+
+    <div className="card" style={{width: "18rem"}}>
+        <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">Github Notifier</h5>
+            <p className="card-text">This content is coming soon!</p>
+            <a href="#" className="btn btn-primary">View Here</a>
+        </div>
+        </div>
+    </div>
+
+    <div className="row">
+        <div className="card" style={{width: "18rem"}}>
+        <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">TODO App</h5>
+            <p className="card-text">This content is coming soon!</p>
+            <a href="#" className="btn btn-primary">View Here</a>
+        </div>
+        </div>
+    
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">Twilo Bot</h5>
+            <p className="card-text">This content is coming soon!</p>
+            <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+        </div>
+
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Meme Generator</h5>
+                <p className="card-text">This content is coming soon!</p>
+                <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+            </div>
+        </div>
+
+
+    <div className="row">
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">RSS Aggregator</h5>
+            <p className="card-text">This content is coming soon!</p>
+            <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+        </div>
+        
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Food Log</h5>
+                <p className="card-text">This content is coming soon!</p>
+                <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                <h5 className="card-title">Google Map</h5>
+                <p className="card-text">This content is coming soon!</p>
+                <a href="#" className="btn btn-primary">View Here</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="row">
+            <p className="Project-headings">Large Projects</p>
+        </div>
+    <div className="row">
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+            <h5 className="card-title">Raspberry Pi Robot</h5>
+            <p className="card-text">This content is coming soon!</p>
+            <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+        </div>
+        
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Bar Hopper App</h5>
+                <p className="card-text">This content is coming soon!</p>
+                <a href="#" className="btn btn-primary">View Here</a>
+            </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                <h5 className="card-title">Online Tournament Generator</h5>
+                <p className="card-text">This content is coming soon!</p>
+                <a href="#" className="btn btn-primary">View Here</a>
+                </div>
+            </div>
+        </div>
+        </main>
+        </>
         </>
     )}
 export default Personal;
--- portfolio/src/components/School.js
+++ portfolio/src/components/School.js
@@ -2,6 +2,41 @@ import React from "react";
 function School() {
     return (
         <>
+            <main className="container">
+
+            <div className="row" >
+                <p className="Project-headings">Capstone Projects</p>
+            </div>
+
+            <div className="row">
+            <div className="card" style={{width: "18rem"}}>
+            <img src="logo 2.png" className="card-img-top" alt="Coming Soon"/>
+            <div className="card-body">
+                <h5 className="card-title">Loqal</h5>
+                <p className="card-text">This Full-Stack application uses multiple third-party API's to help users find unknown musicians as well as listen to days top hits  </p>
+                <a href="https://robertharris95.github.io/RDOProject1/" className="btn btn-primary" target="_blank">View Here</a>
+            </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="logo4.png" className="card-img-top" alt="Coming Soon"/>
+                <div className="card-body">
+                <h5 className="card-title">Contract Bay</h5>
+                <p className="card-text">This full stack application utilizes express.js servers, materialize cli, and mysql databases to create a marketplace for wholesailers can create and purchase contracts to sell their raw products.</p>
+                <a href="https://polar-island-40995.herokuapp.com" className="btn btn-primary" target="_blank">View Here</a>
+                </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="Coming Soon"/>
+                <div className="card-body">
+                    <h5 className="card-title">Capstone 3</h5>
+                    <p className="card-text">This content is coming soon!</p>
+                    <a href="#" className="btn btn-primary">View Here</a>
+                </div>
+                </div>
+            </div>
+        </main>
         </>
     )}
 export default School;
