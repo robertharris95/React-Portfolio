7fe5de8f5d5de2ab7d58f5a436f04d010d71288a
Robert Harris <rlh9@rice.edu>
2020-04-07 21:56:46 -0500

    Added additional projects

--- portfolio/src/components/Personal.js
+++ portfolio/src/components/Personal.js
@@ -41,29 +41,29 @@ function Personal() {
 
     <div className="row">
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+        <img src={process.env.PUBLIC_URL + "/images/Tracker.png" } className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">TODO App</h5>
-            <p className="card-text">This content is coming soon!</p>
-            <a href="#" className="btn btn-primary">View Here</a>
+            <h5 className="card-title">User Tracking Application</h5>
+            <p className="card-text">This application displays the data of users from a data set dynamically in Reactjs</p>
+            <a href=" https://robertharris95.github.io/Homework-15/" className="btn btn-primary">View Here</a>
         </div>
         </div>
     
         <div className="card" style={{width: "18rem"}}>
-            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <img src={process.env.PUBLIC_URL + "/images/github logo.png" } className="card-img-top" alt="..."/>
             <div className="card-body">
-            <h5 className="card-title">Twilo Bot</h5>
-            <p className="card-text">This content is coming soon!</p>
-            <a href="#" className="btn btn-primary">View Here</a>
+            <h5 className="card-title">Team Profile Generator</h5>
+            <p className="card-text">This application lets a user create and manage a team of people from their BASH terminal</p>
+            <a href=" https://github.com/robertharris95/Homework-8" className="btn btn-primary">View Here</a>
             </div>
         </div>
 
         <div className="card" style={{width: "18rem"}}>
-            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <img src={process.env.PUBLIC_URL + "/images/Budget.png" } className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Meme Generator</h5>
-                <p className="card-text">This content is coming soon!</p>
-                <a href="#" className="btn btn-primary">View Here</a>
+                <h5 className="card-title">PWA Budget Tracker</h5>
+                <p className="card-text">This Progressive Web Application allows for users to track their expenses both online and offline</p>
+                <a href="https://safe-hollows-41299.herokuapp.com/" className="btn btn-primary">View Here</a>
             </div>
             </div>
         </div>
@@ -80,11 +80,11 @@ function Personal() {
         </div>
         
         <div className="card" style={{width: "18rem"}}>
-            <img src="https://giordanos.com/wp-content/uploads/coming-soon-v2.png" className="card-img-top" alt="..."/>
+            <img src={process.env.PUBLIC_URL + "/images/Workout.png" } className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Food Log</h5>
-                <p className="card-text">This content is coming soon!</p>
-                <a href="#" className="btn btn-primary">View Here</a>
+                <h5 className="card-title">Workout Log</h5>
+                <p className="card-text">This application helps users to log their exercises via their web browser. They can add new workouts as they see fit and the stats page will display dynamic graphs of their workout progress. </p>
+                <a href="https://dry-lowlands-14027.herokuapp.com/?id=5e7c2240926a8700177cae2c" className="btn btn-primary">View Here</a>
             </div>
             </div>
 
